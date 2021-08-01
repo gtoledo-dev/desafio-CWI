fb0d669d1de3d1a89dcceb286956c511b618d418
gtoledo-dev <glauco_toledo@yahoo.com.br>
2021-08-01 00:07:11 -0300

    created candidates class

--- src/main/java/entities/Candidates.java
+++ src/main/java/entities/Candidates.java
@@ -1,6 +1,97 @@
 package entities;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Candidates {
+	
+	@Id
+	@GeneratedValue
+	@Column
+	private Long id;
+	private String name;
+	private Long cpf;
+	private String email;
+	private String senha;
+	
+	public Candidates(Long id, String name, Long cpf, String email, String senha) {
+
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, email, id, name, senha);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Candidates other = (Candidates) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && Objects.equals(senha, other.senha);
+	}
+
+	@Override
+	public String toString() {
+		return "Candidates [id=" + id + ", name=" + name + ", cpf=" + cpf + ", email=" + email + ", senha=" + senha
+				+ "]";
+	}
+	
 	
 	
 
